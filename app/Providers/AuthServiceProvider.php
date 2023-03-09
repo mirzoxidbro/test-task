41deb43fe18651c3e98c3543690e3adf794cd4ed
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-03-09 18:00:50 +0500

    gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,12 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Company;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +26,40 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('view-company', function(User $user,$company){
+            if($user->id == $company[0]->user_id || $user->hasRole('admin')){
+                return true;
+            }
+        });
+
+        Gate::define('update-company', function(User $user,$company){
+            if($user->id == $company[0]->user_id || $user->hasRole('admin')){
+                return true;
+            }
+        });
+
+        Gate::define('delete-company', function(User $user,$company){
+            if($user->id == $company[0]->user_id || $user->hasRole('admin')){
+                return true;
+            }
+        });
+
+        Gate::define('create-employee', function(User $user){
+            if($user->hasRole('company')){
+                return true;
+            }
+        });
+
+        Gate::define('update-employee', function(User $user){
+            if($user->hasRole('company')){
+                return true;
+            }
+        });
+
+        Gate::define('delete-employee', function(User $user){
+            if($user->hasRole('company')){
+                return true;
+            }
+        });
     }
 }
